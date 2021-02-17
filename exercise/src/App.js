1450c7d25ca4c23ab8f5e3497705b9d961805b95
Mantas <gusmantas@gmail.com>
2021-02-17 10:36:00 +0100

    Added HomePage

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        
-          <Route path="/"></Route>
+          <Route path="/" component={Home}></Route>
           <Route path="/about" component={About}></Route>
       </Router>
     </div>
