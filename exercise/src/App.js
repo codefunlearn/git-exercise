20435208d320415d1e6e59f4b7820fbcc06510ae
Mantas <gusmantas@gmail.com>
2021-02-17 09:39:23 +0100

    Added navbar and its styles

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,9 +1,15 @@
-
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+          <Navbar />
+{/* 
+          <Route path="/"></Route>
+          <Route path="/about"></Route> */}
+      </Router>
     </div>
   );
 }
