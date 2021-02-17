3baa250026f2c46283f6dc5da1f5225bc7e06a93
Mantas <gusmantas@gmail.com>
2021-02-17 09:47:56 +0100

    Added about page

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        {/* 
+        
           <Route path="/"></Route>
-          <Route path="/about"></Route> */}
+          <Route path="/about" component={About}></Route>
       </Router>
     </div>
   );
